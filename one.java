2cbc4187f87e1623f1f7c5863c62889a4104dd03
assasinanuj <anuj.agrawal_cs15@gla.ac.in>
2018-01-10 21:33:42 +0430

    Create one.java

---
+++ one.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class One
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<0)
+		{
+			System.out.println("Negative");
+		}
+		else
+		{
+			System.out.println("Positive");
+		}
+	}
+}
